b12e07e65fc0b8be9dbea53eb8ff2d0bc10b9f87
huan <yang_95@126.com>
2023-09-24 13:12:31 +0800

    完成任务4

--- src/index.js
+++ src/index.js
@@ -7,3 +7,9 @@ function f() {
 
 console.log(f());
 console.log("hi2");
+
+const body = document.body;
+
+const h1 = document.createElement("h1");
+h1.innerHTML = "hello huan2";
+body.appendChild(h1);
