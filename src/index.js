5ded37399e52706887a041829dfba61664cb4b15
huan <15084920063@126.com>
2023-09-26 00:12:15 +0800

    完成用户性能优化

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import axios from "axios";
 // import ReactDom from "react-dom";
 import "./style.css";
 import "./reset.css";
+import jpg from "./images/1.jpg";
 
 console.log(axios);
 // console.log(vue);
@@ -11,5 +12,22 @@ console.log(axios);
 // console.log(ReactDom);
 
 const h1 = document.createElement("h1");
+const btn = document.createElement("button");
 h1.innerHTML = "hello";
+
 document.body.appendChild(h1);
+document.body.appendChild(btn);
+
+btn.innerHTML = "懒加载";
+btn.addEventListener("click", () => {
+  import("./a.js").then(
+    ({ a }) => {
+      console.log(a.name);
+    },
+    () => {}
+  );
+});
+const image = document.createElement("img");
+console.log(jpg);
+image.src = jpg;
+document.body.appendChild(image);
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,45 @@
 const path = require("path");
 
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { WebpackManifestPlugin } = require("webpack-manifest-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 console.log(MiniCssExtractPlugin);
 module.exports = {
   entry: path.resolve(__dirname, "./src/index.js"),
-  mode: "development",
-  output: {
-    path: path.resolve(__dirname, "./dist"),
-    filename: "main.js",
+  mode: "production",
+  performance: {
+    hints: false,
   },
-  plugins: [new MiniCssExtractPlugin()],
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].[contenthash].js",
+    clean: true,
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash].css",
+      chunkFilename: "[id].[contenthash].css",
+    }),
+    new WebpackManifestPlugin({}),
+    new HtmlWebpackPlugin({
+      title: "huan",
+      template: "./src/index.html",
+    }),
+  ],
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ["MiniCssExtractPlugin.loader", "css-loader"],
-        // use: ["style-loader", "css-loader"],
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: "file-loader",
+          },
+        ],
       },
     ],
   },
